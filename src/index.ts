849a902522e1a03bfa960dcd686ce3ddc949ece4
José Miguel Pérez Méndez <2061598+JotaEmePM@users.noreply.github.com>
2024-05-10 18:10:56 -0400

    Feat: tarea_4_it1

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import app from "./server.js";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log("server started");
+  console.log(`server started at ${port}`);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 import express from "express";
+import fs from 'fs'
+
 import { dividir, multiplicar, restar, sumar } from "./calcular.js";
+
+//dotenv.config();
 
 const app = express();
 app.use(express.json());
@@ -22,6 +26,13 @@ app.post("/calcular", (req, res) => {
   return res.send({ resultado: "hola mundo" });
 });
 
+app.get("/info", (req, res) => {
+  return res.send(`AMBIENTE: ${process.env.AMBIENTE}`)
+})
 
+app.get('/api', (req, res) => {
+  const apikey = fs.readFileSync(process.env.API_KEY || '', 'utf8');
+  return res.send(`APIKEY: ${apikey}`)
+})
 
 export default app;
